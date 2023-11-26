d309148518b2499f8c37fd78b0420a90b91d81e7
Kittiphon Kamnuan <kittiphon.kam@dome.tu.ac.th>
2023-11-26 19:38:50 +0700

    make welcome page

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function viewProfile(profileId) {
   console.log(`Viewing profile ${profileId}`);
 }
+
+document.getElementById('exploreBtn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default behavior of the link
+
+  // Add the slide-in effect to the container
+  document.querySelector('.container').classList.add('slide-in');
+
+  // Simulate a delay (you can adjust this)
+  setTimeout(function() {
+      // Redirect to the explore page after the slide-in effect
+      window.location.href = 'html/homepage.html';
+  }, 500); // 500 milliseconds, matches the transition duration in CSS
+});
