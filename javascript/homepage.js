835724de0c4eb295c461e629d7323d79f39d5d93
Kittiphon Kamnuan <kittiphon.kam@dome.tu.ac.th>
2023-11-29 17:59:59 +0700

    add activity

---
+++ javascript/homepage.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const characters = document.querySelectorAll('.character');
+
+  characters.forEach(character => {
+    character.addEventListener('click', function () {
+      const index = this.getAttribute('data-index');
+      alert(`You selected character ${parseInt(index) + 1}`);
+
+      // Redirect to activity page based on the selected character
+      switch (parseInt(index)) {
+        case 0:
+          window.location.href = 'activity1.html';
+          break;
+        case 1:
+          window.location.href = 'activity2.html';
+          break;
+        case 2:
+          window.location.href = 'activity3.html';
+          break;
+        // Add more cases if you have additional characters
+        default:
+          // Handle the default case (optional)
+      }
+    });
+  });
+});
